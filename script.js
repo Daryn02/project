7098d3e3acaf9bbdefa064b30dfdeec50584a251
Daryn <example@gmail.com>
2022-11-08 02:28:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//alert("Hello, world!");
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18")
+//console.log(answer);
+
+//const answers = []; 
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваше фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+//console.log(typeof(answers));
+
+//const category = "toys";
+//console.log(`https://someurl.com/${category}/5`);
+//const user = prompt("Как ваше имя?", "")
+//alert(`Hello, ${user}`);
+
+//let firstnameUser = prompt('Ваше имя: '); 
+//let lastnameUser = prompt('Ваша фамилия: ');
+//let gender = prompt('Какой у вас пол: ');
+
+let firstNameOfUser = form.elements.firstName;
+let lastNameOfUser = form.elements.lastName;
+let gender = form.elements.gender;
+
+alert(form.elements.firstName, form.elements.lastName, form.elements.gender);
